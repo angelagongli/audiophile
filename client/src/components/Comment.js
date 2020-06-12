cd25d1e52d5b802fb732540066440d7a32115b04
angela <angelagongli@gmail.com>
2020-06-12 18:18:10 -0500

    add edit functionality to Comment.js

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,30 +1,76 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import OpenEditBtn from "./OpenEditBtn";
 import EditBtn from "./EditBtn";
+import BackBtn from "./BackBtn";
 import DeleteBtn from "./DeleteBtn";
 import UserContext from "../utils/userContext";
-import { Grid, Card, CardActionArea, CardActions, CardContent } from '@material-ui/core';
+import { Grid, Card, CardActionArea, CardActions, CardContent, TextField } from '@material-ui/core';
+
+function CommentBox(props) {
+  const [formObject, setFormObject] = useState({});
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ [name]: value });
+  };
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    props.editComment(props.id, { text : formObject.edit });
+    props.onFinish();
+  };
+
+  return (
+    <div className="comment-box">
+      <div className="form">
+        <form>
+          <TextField
+            name="edit"
+            id="edit"
+            onChange={handleInputChange}
+            label="Edit this comment"
+            variant="outlined"
+            defaultValue={props.text}
+            fullWidth />
+          <span className="button-span">
+            <EditBtn disabled={!(formObject.edit)} onClick={handleFormSubmit} />
+            <BackBtn onClick={props.onFinish} />
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 function Comment(props) {  
+  const [inEdit, setInEdit] = useState(false);
   const { username, userID } = useContext(UserContext);  
   
+  const handleEditStart = () => {
+    setInEdit(true);
+  }
+
+  const handleEditFinish = () => {
+    setInEdit(false);
+  }
+
   return (
     <Grid container spacing={2}>
       <Grid item sm={10}>
-        <Card className="card" style={props.user === userID ? {backgroundColor : "lightskyblue"} : {backgroundColor : "lightpink"}}>
+        <Card className="card" style={props.user === userID ?
+          {backgroundColor: "lightskyblue"} : {backgroundColor: "lightpink"}}>
           <CardActionArea>
             <h1>
-              {props.user === props.makerID ? props.maker : props.joiner } @ {Math.floor(props.time / 60) + ":" + 
-              (Math.floor(props.time % 60) < 10 ? "0" : "") + Math.floor(props.time % 60)}
+              {props.user === props.makerID ? props.maker : props.joiner } @ {props.time}
             </h1>
             <CardContent>
-              <p>
-                {props.text}
-              </p>
+              {inEdit ? <CommentBox onFinish={handleEditFinish} {...props} /> : 
+              <p>{props.text}</p>}
             </CardContent>
           </CardActionArea>
           {props.user === userID ? 
           <CardActions>
-            <EditBtn onClick={() => props.editComment(props.id, { text : "updated text" })} />
+            <OpenEditBtn onClick={handleEditStart} />
             <DeleteBtn onClick={() => props.deleteComment(props.id)} />
           </CardActions> : ""}
         </Card>
